117d52d98ecfa119ed0b1f933e0017c71446fa86
Carlo <jancarlodelmoro@gmail.com>
2023-08-05 18:19:29 +0800

    Feat: Added Tasks

---
+++ client/src/pages/Dashboard.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { useUserDataStore } from '@/stores/userData';
+import axios from 'axios';
+
+import NavBar from '../components/NavBar.vue'
+import TaskComponent from '../components/TaskComponent.vue';
+
+const data = useUserDataStore();
+
+//fetch user data and save it UserData store (variable)
+axios.get(`${ import.meta.env.VITE_SITE_LINK }/api/v1/users/${ localStorage.getItem('userId') }`).then((dbResponse) => {
+    data.username = dbResponse.data.user.username;
+    data.email = dbResponse.data.user.email;
+});
+
+</script>
+
+<template>
+    <NavBar />
+
+    <div class="dashboard-container">
+        <div class="controls">
+
+        </div>
+
+        <!-- In Progress Tasks -->
+        <TaskComponent>
+            <template #title> In Progress </template>
+        </TaskComponent>
+
+        <!-- In Completed -->
+        <TaskComponent>
+            <template #title> Completed </template>
+        </TaskComponent>
+    </div>
+</template>
+
+<style scoped>
+    .dashboard-container {
+        height: 100%;
+        background: #F9F9F9;
+        padding: 50px 100px;
+        margin: auto;
+    }
+</style>
