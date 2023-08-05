1d4806f0faaaf7ccd1f809abb9098cc2dae5d3a0
Carlo <jancarlodelmoro@gmail.com>
2023-08-05 18:19:58 +0800

    Feat: Fetch and display tasks from database

---
+++ client/src/components/TaskComponent.vue
@@ -0,0 +1,83 @@
+<script setup>
+    import { useUserDataStore } from '@/stores/userData';
+    import { ref } from 'vue';
+
+    import axios from 'axios';
+
+    const data = useUserDataStore();
+
+    const taskList = ref([])
+
+//fetch user tasks and save it UserData store (variable)
+    axios.get(`${ import.meta.env.VITE_SITE_LINK }/api/v1/users/${ localStorage.getItem('userId') }/tasks`).then((dbResponse) => {
+        data.tasks = dbResponse.data.tasksList;
+        taskList.value = data.tasks;
+    });
+
+</script>
+
+<template>
+    <div class="container">
+        <div class='title'>
+            <h3>
+                <slot name="title"></slot>
+            </h3>
+        </div>
+
+        <div class='task-list'>
+            <div v-for="task in taskList" class="tasks">
+                <input
+                    type="checkbox"
+                >
+                <p> {{ task.data.name }} </p>
+                <p> {{ task.data.description }} </p>
+                <button class="edit-btn"> Edit </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+    .container {
+        height: 45vh;
+        background: var(--white);
+        border-radius: 5px;
+        box-shadow: 0 0 10px #D3D3D3;
+        padding: 20px;
+        margin: 30px 0;
+        overflow-y: auto;
+    }
+
+    h3 {
+        font-family: Arial, Helvetica, sans-serif;
+        font-weight: normal;
+    }
+
+    .task-list {
+        margin: 0 20px;
+    }
+
+    .tasks {
+        display: flex;
+        align-items: center;
+        background: #D3D3D3;
+        border-radius: 5px;
+        box-shadow: 0 0 5px #D3D3D3;
+        padding: 10px;
+        margin: 2px 0;
+    }
+
+    .tasks input {
+        margin-right: 10px;
+    }
+
+    .tasks p {
+        width: 100%;
+        text-overflow: ellipsis;
+    }
+
+    .edit-btn {
+        padding: 5px;
+    }
+</style>
