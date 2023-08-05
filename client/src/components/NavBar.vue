ac69ba20fa067e43dd55402374e6c430f48ba8c6
Carlo <jancarlodelmoro@gmail.com>
2023-08-05 15:02:02 +0800

    Feat: Added Navbar

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="navbar-container">
+        <img
+            src=""
+            alt="logo"
+        >
+    </div>
+</template>
+
+<style scoped>
+    .navbar-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: var(--main-color);
+        padding: 20px;
+    }
+</style>
