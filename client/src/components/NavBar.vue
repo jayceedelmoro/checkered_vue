1938fa696f0aafd6f146863bcdd96b1ddcd4e69e
Carlo <jancarlodelmoro@gmail.com>
2023-08-05 15:11:24 +0800

    Feat: Added user's username in the navbar

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,9 +1,18 @@
+<script setup>
+
+    import { useUserDataStore } from '@/stores/userData';
+    const data = useUserDataStore();
+
+</script>
+
 <template>
     <div class="navbar-container">
         <img
             src=""
             alt="logo"
+            class="logo"
         >
+        <p class="username"> {{ data.username }}</p>
     </div>
 </template>
 
@@ -12,7 +21,20 @@
         display: flex;
         justify-content: center;
         align-items: center;
+        width: 100%;
         background-color: var(--main-color);
         padding: 20px;
+    }
+
+    .navbar-container > * {
+        width: 100%;;
+    }
+
+    .username {
+        color: var(--white);
+        font-size: 20px;
+        text-transform: uppercase;
+        text-align: right;
+        margin: 0;
     }
 </style>
